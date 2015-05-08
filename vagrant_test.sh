347837c3fa06bb48243e30c16913adca7edb24cb
Karanbir Singh <kbsingh@karan.org>
2015-05-09 00:50:52 +0100

    dont block on pending updates, just indicate how many are due

--- vagrant_test.sh
+++ vagrant_test.sh
@@ -4,10 +4,11 @@ cd ~/sync/
 vagrant init lalatendum/centos7-docker
 vagrant up
 UpdtPkgs=$(vagrant ssh -c "sudo yum -d0 list updates | wc -l")
-if [ $UpdtPkgs -gt 10 ]; then
-    echo 'More than 10 packages due an update!'
-    exit 1
-else
+echo 'Updates due :' ${UpdtPkgs} 
+#if [ $UpdtPkgs -gt 10 ]; then
+#    echo 'More than 10 packages due an update!'
+#    exit 1
+#else
     vagrant ssh -c "cd sync; sudo ./runtests.sh "
     # the $? check here isnt going to work since it will be the ssh exit, not the
     # script exit 
@@ -17,4 +18,4 @@ else
     else
       exit 0
     fi
-fi
+#fi
